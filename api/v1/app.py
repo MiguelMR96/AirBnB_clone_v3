f00d0c21f1f35daf51e8b2cf60f95e2faf343f8e
Victor Zuluaga <zrvictor@outlook.com>
2021-01-27 17:57:59 -0500

    404 custom page

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 
 
-from flask import Flask, Blueprint, render_template
+from flask import Flask, make_response, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -19,6 +19,12 @@ def teardown_app(exception):
     """
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 custom page
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     if getenv('HBNB_API_HOST') is None:
